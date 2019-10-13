83d3313b473ce71fe4cb0bad43e1792493013dc3
Aswin Raghuprasad <aswinrprasad@gmail.com>
2019-10-13 23:44:40 +0530

    Python File

--- Linear Regression/linear+.py
+++ Linear Regression/linear+.py
@@ -1,46 +1,12 @@
 
-# coding: utf-8
-
-# In[84]:
-
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-# In[85]:
-
-
 df=pd.read_csv('weather.csv')
-
-
-# In[86]:
-
-
 df.head()
-
-
-# In[87]:
-
-
 new_data = df[['MaxTemp', 'Rainfall','Humidity3pm','Cloud9am','RISK_MM']]
-
-
-# In[88]:
-
-
-new_data
-
-
-# In[89]:
-
-
 new_data.head()
-
-
-# In[90]:
-
 
 from sklearn.model_selection import train_test_split  
 from sklearn.preprocessing import StandardScaler
@@ -58,52 +24,8 @@ y=pd.Series(np.where(y == 'Yes', 1, 0),y)
 
 x = norm(x)
 y = norm(y.reshape(-1,1))
-print(y)
-
-
-# In[91]:
-
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20)
-
-
-# In[92]:
-
-
-x_train
-
-
-# In[93]:
-
-
-y_test
-
-
-# In[94]:
-
-
-x_train.shape
-
-
-# In[95]:
-
-
-x_test.shape
-
-
-# In[96]:
-
-
-y_train.shape
-
-
-# In[97]:
-
-
-y_test.shape
-
-
-# In[98]:
 
 
 def cost(x,theta,y):
@@ -133,14 +55,7 @@ def gradient_descent(alpha, x,y,norma,max_iter=1500):
     return theta
 
 
-# In[111]:
-
-
-#theta = gradient_descent(0.001, x_train, y_train,False)
-
-
-# In[100]:
-
+theta = gradient_descent(0.001, x_train, y_train,False)
 
 temp = np.ones(len(x_test))
 x_test = np.vstack((temp.T, x_test.T))
@@ -148,9 +63,4 @@ y_pred = np.dot(x_test.T, theta)
 mse = (1/(len(x_test))) * np.square(y_pred-y_test).sum()
 print("Mean squared error",mse)
 
-
-# In[101]:
-
-
-theta
 
